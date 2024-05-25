34232e671dc482bb56d93e89d5a3a3c2f00ce918
Han <qw753954@gmail.com>
2024-05-25 17:36:56 +0800

    modify base

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     VueDevTools(),
   ],
+  base: '/vite-deploy-test/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
