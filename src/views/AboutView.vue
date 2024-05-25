7e0961f1045566d7263c5cd730eb18779025e302
Han <qw753954@gmail.com>
2024-05-25 17:21:44 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ userData }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userData: {}
+    }
+  },
+  methods: {
+    getUser() {
+      console.log(import.meta.env.VITE_TEXT)
+      console.log(import.meta.env.VITE_PATH)
+
+      this.$http
+        .get(`${import.meta.env.VITE_PATH}?gender=female`)
+        .then((res) => {
+          console.log(res)
+          this.userData = res.data.results
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  },
+  created() {
+    this.getUser()
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
